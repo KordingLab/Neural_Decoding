65bf810afdaa787b3a1b82896307f224fbe7bb41
jglaser2 <joshglaser88@gmail.com>
2017-01-26 18:14:52 -0600

    Added WC and KF to decoder_funcs

--- Decoder_funcs.py
+++ Decoder_funcs.py
@@ -95,5 +95,86 @@ def xgb_predict(models,X_test):
     return y_test_pred
 
 
+### Wiener Cascade (Linear Nonlinear model)
+def wiener_casc_model(X_flat_train,y_train,deg=3):
+    num_outputs=y_train.shape[1]
+    models=[]
+    for i in range(num_outputs):
+        regr = linear_model.LinearRegression()
+        regr.fit(X_flat_train, y_train[:,i]) #Fit linear
+        y_train_pred_lin=regr.predict(X_flat_train)
+        p=np.polyfit(y_train_pred_lin,y_train[:,i],deg) #Fit nonlinear
+        models.append([regr,p])
+    return models
 
-    
+def wiener_casc_predict(models,X_flat_test):
+    num_outputs=len(models)
+    y_test_pred_ln=np.empty([X_flat_test.shape[0],num_outputs])
+    for i in range(num_outputs):
+        [regr,p]=models[i]
+        #Predictions on test set
+        y_test_pred_lin=regr.predict(X_flat_test)
+        y_test_pred_ln[:,i]=np.polyval(p,y_test_pred_lin)
+    return y_test_pred_ln
+
+
+### Kalman Filter ###
+def kf_model(zs,xs):
+    #xs are the state (here, the variable we're predicting)
+    #zs are the observed variable (neural data here)
+
+
+    #Reformat matrices
+    X=np.matrix(xs.T)
+    Z=np.matrix(zs.T)
+
+    #number of time bins (after reformatting for the lag)
+    nt=X.shape[1]
+
+    # Compute the variables required for filtering, according to Wu et al
+    X2 = X[:,1:]
+    X1 = X[:,0:nt-1]
+    # The least-squares-optimal transformation from x_i to x_(i+1)
+    A=X2*X1.T*inv(X1*X1.T)
+    W=(X2-A*X1)*(X2-A*X1).T/(nt-1)
+
+    # The least-squares-optimal transformation from x_i to z_i
+    # (the transformation from position to spikes)
+    H = Z*X.T*(inv(X*X.T))
+    Q = ((Z - H*X)*((Z - H*X).T)) / nt
+    params=[A,W,H,Q]
+    return params
+
+
+def kf_predict(params,zs,xs):
+    #xs are the state (here, the variable we're predicting)
+    #zs are the observed variable (neural data here)
+
+    #Extract parameters
+    A,W,H,Q=params
+
+    #Reformat matrices
+    X=np.matrix(xs.T)
+    Z=np.matrix(zs.T)
+
+    #Initialize
+    num_states=X.shape[0]
+    states=np.empty(X.shape)
+    P_m=np.matrix(np.zeros([num_states,num_states]))
+    P=np.matrix(np.zeros([num_states,num_states]))
+    state=X[:,0]
+    states[:,0]=np.copy(np.squeeze(state))
+
+    for t in range(X.shape[1]-1):
+#         P_m=np.copy(A*P*A.T+W)
+#         state_m=np.copy(A*state)
+
+        P_m=A*P*A.T+W
+        state_m=A*state
+
+        K=P_m*H.T*inv(H*P_m*H.T+Q)
+        P=(np.matrix(np.eye(num_states))-K*H)*P_m
+        state=state_m+K*(Z[:,t+1]-H*state_m)
+#         states[:,t+1]=np.copy(np.squeeze(state))
+        states[:,t+1]=np.squeeze(state)
+    return states.T
