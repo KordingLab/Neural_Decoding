6b361860045503027e316a506da4a1335e3696c2
jglaser2 <joshglaser88@gmail.com>
2017-01-30 11:51:32 -0600

    Add DNN

--- Decoder_funcs.py
+++ Decoder_funcs.py
@@ -30,6 +30,17 @@ def lin_reg_model(X_flat_train,y_train):
     regr.fit(X_flat_train, y_train) #Train
     return regr
 
+##Feedforward NN
+def DNN_model(X_train,y_train,units=400,dropout=0,num_epochs=10,verbose=0):
+    model=Sequential()
+    model.add(Dense(units,input_dim=X_train.shape[1]))
+    if dropout!=0:
+        model.add(Dropout(dropout))
+    model.add(Dense(y_train.shape[1],init='uniform'))
+    model.compile(loss='mse',optimizer='rmsprop',metrics=['accuracy'])
+    model.fit(X_train,y_train,nb_epoch=num_epochs,verbose=verbose)
+    return model
+
 ## Simple RNN
 def SimpleRNN_model(X_train,y_train,units=400,dropout=0,num_epochs=10,verbose=0):
     model=Sequential()
