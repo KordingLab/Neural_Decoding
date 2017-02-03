453db5651fff738d859b825d862d284ec3077af8
jglaser2 <joshglaser88@gmail.com>
2017-02-03 17:52:53 -0600

    Decoder with classes updates

--- decoders.py
+++ decoders.py
@@ -1,11 +1,12 @@
-#### IMPORT PACKAGES #####
+############### IMPORT PACKAGES ##################
+
 import numpy as np
 from numpy.linalg import inv as inv #Used in kalman filter
 from sklearn import linear_model #For linear regression (wiener filter)
 #Import XGBoost if the package is installed
 try:
     import xgboost as xgb #For xgboost
-except ImportError, e:
+except ImportError:
     print("\nWARNING: Xgboost package is not installed. You will be unable to use the xgboost decoder")
     pass
 
@@ -15,77 +16,253 @@ except ImportError, e:
 try:
     from keras.models import Sequential
     from keras.layers import Dense, LSTM, SimpleRNN, GRU, Activation, Dropout
-except ImportError, e:
+except ImportError:
     print("\nWARNING: Keras package is not installed. You will be unable to use all neural net decoders")
     pass
 
 
 
-""" ###### DECODER FUNCTIONS ####### """
+##################### DECODER FUNCTIONS ##########################
 
 
-class WienerFilter(object):
-    """Class for the Wiener Filter Decoder
+
+##################### WIENER FILTER ##########################
+
+class WienerFilterDecoder(object):
+
+    """
+    Class for the Wiener Filter Decoder
 
     There are no parameters to set.
     """
+
     def __init__(self):
         return
 
+
     def fit(self,X_flat_train,y_train):
+
+        """
+        Train Wiener Filter Decoder
+
+        Parameters
+        ----------
+        X_flat_train: numpy 2d array of shape [n_samples,n_features]
+            This is the neural data.
+            See example file for an example of how to format the neural data correctly
+
+        y_train: numpy 2d array of shape [n_samples, n_outputs]
+            This is the outputs that are being predicted
+        """
+
         self.model=linear_model.LinearRegression() #Initialize linear regression model
         self.model.fit(X_flat_train, y_train) #Train the model
 
-    def predict(self,x):
-        return self.model.predict(x)
+
+    def predict(self,X_flat_test):
+
+        """
+        Predict outcomes using trained Wiener Cascade Decoder
+
+        Parameters
+        ----------
+        X_flat_test: numpy 2d array of shape [n_samples,n_features]
+            This is the neural data being used to predict outputs.
+
+        Returns
+        -------
+        y_test_predicted: numpy 2d array of shape [n_samples,n_outputs]
+            The predicted outputs
+        """
+
+        y_test_predicted=self.model.predict(X_flat_test) #Make predictions
+        return y_test_predicted
 
 
-class WienerCascade(object):
-    """Class for the Wiener Cascade Decoder
+
+
+##################### WIENER CASCADE ##########################
+
+class WienerCascadeDecoder(object):
+
+    """
+    Class for the Wiener Cascade Decoder
 
     Parameters
     ----------
     degree: integer, optional, default 3
-        The degree of the polynomial used for the nonlinearity
-
+        The degree of the polynomial used for the static nonlinearity
     """
+
     def __init__(self,degree=3):
          self.degree=degree
 
+
     def fit(self,X_flat_train,y_train):
-        # self.fitmodel=Decoder_funcs.wiener_casc_model(x,y,self.degree)
-        num_outputs=y_train.shape[1]
-        models=[]
-        for i in range(num_outputs):
-            regr = linear_model.LinearRegression()
+
+        """
+        Train Wiener Cascade Decoder
+
+        Parameters
+        ----------
+        X_flat_train: numpy 2d array of shape [n_samples,n_features]
+            This is the neural data.
+            See example file for an example of how to format the neural data correctly
+
+        y_train: numpy 2d array of shape [n_samples, n_outputs]
+            This is the outputs that are being predicted
+        """
+
+        num_outputs=y_train.shape[1] #Number of outputs
+        models=[] #Initialize list of models (there will be a separate model for each output)
+        for i in range(num_outputs): #Loop through outputs
+            #Fit linear portion of model
+            regr = linear_model.LinearRegression() #Call the linear portion of the model "regr"
             regr.fit(X_flat_train, y_train[:,i]) #Fit linear
-            y_train_pred_lin=regr.predict(X_flat_train)
-            p=np.polyfit(y_train_pred_lin,y_train[:,i],self.degree) #Fit nonlinear
+            y_train_predicted_linear=regr.predict(X_flat_train) # Get outputs of linear portion of model
+            #Fit nonlinear portion of model
+            p=np.polyfit(y_train_predicted_linear,y_train[:,i],self.degree)
+            #Add model for this output (both linear and nonlinear parts) to the list "models"
             models.append([regr,p])
         self.model=models
 
+
     def predict(self,X_flat_test):
-        models=self.model
-        num_outputs=len(models)
-        y_test_pred_ln=np.empty([X_flat_test.shape[0],num_outputs])
-        for i in range(num_outputs):
-            [regr,p]=models[i]
+
+        """
+        Predict outcomes using trained Wiener Cascade Decoder
+
+        Parameters
+        ----------
+        X_flat_test: numpy 2d array of shape [n_samples,n_features]
+            This is the neural data being used to predict outputs.
+
+        Returns
+        -------
+        y_test_predicted: numpy 2d array of shape [n_samples,n_outputs]
+            The predicted outputs
+        """
+
+        num_outputs=len(self.model) #Number of outputs being predicted. Recall from the "fit" function that self.model is a list of models
+        y_test_predicted=np.empty([X_flat_test.shape[0],num_outputs]) #Initialize matrix that contains predicted outputs
+        for i in range(num_outputs): #Loop through outputs
+            [regr,p]=self.model[i] #Get the linear (regr) and nonlinear (p) portions of the trained model
             #Predictions on test set
-            y_test_pred_lin=regr.predict(X_flat_test)
-            y_test_pred_ln[:,i]=np.polyval(p,y_test_pred_lin)
-        return y_test_pred_ln
+            y_test_predicted_linear=regr.predict(X_flat_test) #Get predictions on the linear portion of the model
+            y_test_predicted[:,i]=np.polyval(p,y_test_predicted_linear) #Run the linear predictions through the nonlinearity to get the final predictions
+        return y_test_predicted
 
-##Feedforward Neural network
-class DNN(object):
 
-    """Class for the feedforward neural network decoder
+
+##################### DENSE (FULLY-CONNECTED) NEURAL NETWORK ##########################
+
+class DenseNNDecoder(object):
+
+    """
+    Class for the dense (fully-connected) neural network decoder
 
     Parameters
     ----------
-    num_layers: integer, optional, default 1
-        Number of hidden layers in the neural network
-        0 would mean that the input is directly connected to the output
 
+    units: integer or vector of integers, optional, default 400
+        This is the number of hidden units in each layer
+        If you want a single layer, input an integer (e.g. units=400 will give you a single hidden layer with 400 units)
+        If you want multiple layers, input a vector (e.g. units=[400,200]) will give you 2 hidden layers with 400 and 200 units, repsectively.
+        The vector can either be a list or an array
+
+    dropout: decimal, optional, default 0
+        Proportion of units that get dropped out
+
+    num_epochs: integer, optional, default 10
+        Number of epochs used for training
+
+    verbose: binary, optional, default=0
+        Whether to show progress of the fit after each epoch
+    """
+
+    def __init__(self,units=400,dropout=0,num_epochs=10,verbose=0):
+         self.dropout=dropout
+         self.num_epochs=num_epochs
+         self.verbose=verbose
+
+         #If "units" is an integer, put it in the form of a vector
+         try: #Check if it's a vector
+             units[0]
+         except: #If it's not a vector, create a vector of the number of units for each layer
+             units=[units]
+         self.units=units
+
+         #Determine the number of hidden layers (based on "units" that the user entered)
+         self.num_layers=len(units)
+
+    def fit(self,X_flat_train,y_train):
+
+        """
+        Train DenseNN Decoder
+
+        Parameters
+        ----------
+        X_flat_train: numpy 2d array of shape [n_samples,n_features]
+            This is the neural data.
+            See example file for an example of how to format the neural data correctly
+
+        y_train: numpy 2d array of shape [n_samples, n_outputs]
+            This is the outputs that are being predicted
+        """
+
+        model=Sequential() #Declare model
+        #Add first hidden layer
+        model.add(Dense(self.units[0],input_dim=X_flat_train.shape[1])) #Add dense layer
+        model.add(Activation('tanh')) #Add nonlinear (tanh) activation
+        # if self.dropout!=0:
+        if self.dropout!=0: model.add(Dropout(self.dropout))  #Dropout some units if proportion of dropout != 0
+
+        #Add any additional hidden layers (beyond the 1st)
+        for layer in range(self.num_layers-1): #Loop through additional layers
+            model.add(Dense(self.units[layer+1])) #Add dense layer
+            model.add(Activation('tanh')) #Add nonlinear (tanh) activation
+            if self.dropout!=0: model.add(Dropout(self.dropout)) #Dropout some units if proportion of dropout != 0
+
+        #Add dense connections to all outputs
+        model.add(Dense(y_train.shape[1])) #Add final dense layer (connected to outputs)
+
+        #Fit model (and set fitting parameters)
+        model.compile(loss='mse',optimizer='rmsprop',metrics=['accuracy']) #Set loss function and optimizer
+        model.fit(X_flat_train,y_train,nb_epoch=self.num_epochs,verbose=self.verbose) #Fit the model
+        self.model=model
+
+
+    def predict(self,X_flat_test):
+
+        """
+        Predict outcomes using trained DenseNN Decoder
+
+        Parameters
+        ----------
+        X_flat_test: numpy 2d array of shape [n_samples,n_features]
+            This is the neural data being used to predict outputs.
+
+        Returns
+        -------
+        y_test_predicted: numpy 2d array of shape [n_samples,n_outputs]
+            The predicted outputs
+        """
+
+        y_test_predicted = self.model.predict(X_flat_test) #Make predictions
+        return y_test_predicted
+
+
+
+
+##################### SIMPLE RECURRENT NEURAL NETWORK ##########################
+
+class SimpleRNNDecoder(object):
+
+    """
+    Class for the simple recurrent neural network decoder
+
+    Parameters
+    ----------
     units: integer, optional, default 400
         Number of hidden units in each layer
 
@@ -99,28 +276,57 @@ class DNN(object):
         Whether to show progress of the fit after each epoch
     """
 
-    def __init__(self,num_layers=1,units=400,dropout=0,num_epochs=10,verbose=0):
-         self.num_layers=num_layers
+    def __init__(self,units=400,dropout=0,num_epochs=10,verbose=0):
          self.units=units
          self.dropout=dropout
-         self.num_epochs=10
+         self.num_epochs=num_epochs
          self.verbose=verbose
 
+
     def fit(self,X_train,y_train):
-        model=Sequential()
-        model.add(Dense(self.units,input_dim=X_train.shape[1],init='uniform'))
-        model.add(Activation('tanh'))
-        if self.dropout!=0:
-            model.add(Dropout(self.dropout))
-        for layer in range(self.num_layers-1):
-            model.add(Dense(self.units,init='uniform'))
-            model.add(Activation('tanh'))
-            if self.dropout!=0:
-                model.add(Dropout(self.dropout))
-        model.add(Dense(y_train.shape[1],init='uniform'))
-        model.compile(loss='mse',optimizer='rmsprop',metrics=['accuracy'])
-        model.fit(X_train,y_train,nb_epoch=self.num_epochs,verbose=self.verbose)
+
+        """
+        Train SimpleRNN Decoder
+
+        Parameters
+        ----------
+        X_train: numpy 3d array of shape [n_samples,n_time_bins,n_neurons]
+            This is the neural data.
+            See example file for an example of how to format the neural data correctly
+
+        y_train: numpy 2d array of shape [n_samples, n_outputs]
+            This is the outputs that are being predicted
+        """
+
+        model=Sequential() #Declare model
+        #Add recurrent layer
+        model.add(SimpleRNN(self.units,input_shape=(X_train.shape[1],X_train.shape[2]),dropout_W=self.dropout,dropout_U=self.dropout)) #Within recurrent layer, include dropout
+        if self.dropout!=0: model.add(Dropout(self.dropout)) #Dropout some units (recurrent layer output units)
+
+        #Add dense connections to output layer
+        model.add(Dense(y_train.shape[1]))
+
+        #Fit model (and set fitting parameters)
+        model.compile(loss='mse',optimizer='rmsprop',metrics=['accuracy']) #Set loss function and optimizer
+        model.fit(X_train,y_train,nb_epoch=self.num_epochs,verbose=self.verbose) #Fit the model
         self.model=model
 
+
     def predict(self,X_test):
-        return self.model.predict(X_test)
+
+        """
+        Predict outcomes using trained SimpleRNN Decoder
+
+        Parameters
+        ----------
+        X_test: numpy 3d array of shape [n_samples,n_time_bins,n_neurons]
+            This is the neural data being used to predict outputs.
+
+        Returns
+        -------
+        y_test_predicted: numpy 2d array of shape [n_samples,n_outputs]
+            The predicted outputs
+        """
+
+        y_test_predicted = self.model.predict(X_test) #Make predictions
+        return y_test_predicted
